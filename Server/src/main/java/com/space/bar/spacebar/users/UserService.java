b1b75b776a4d2db7b3c88bfef534446d8daabb43
Tobias Beeh <tobias.beeh@t-online.de>
2017-11-18 00:09:47 +0100

    fixed server

--- Server/src/main/java/com/space/bar/spacebar/users/UserService.java
+++ Server/src/main/java/com/space/bar/spacebar/users/UserService.java
@@ -2,11 +2,12 @@ package com.space.bar.spacebar.users;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class UserService {
-    private Set<String> users;
+    private Set<String> users = new HashSet<>();
 
     public boolean createUser(String username) {
         return users.add(username);
