c98c8a999be0541ff56ee55657171ffc2fe718ba
Flakebi <flakebi@t-online.de>
2017-11-18 21:17:11 +0100

    Rename android project

--- ProjectSPACEbar/Droid/MainActivity.cs
+++ ProjectSPACEbar/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace ProjectSPACEbar.Droid
 {
-    [Activity(Label = "ProjectSPACEbar.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ProjectSPACEbar", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
